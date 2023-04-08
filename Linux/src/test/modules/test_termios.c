6e4d56bc9691567057f581000d161ac8adcc637b
keunbum <rkeunbum@gmail.com>
2023-04-08 12:52:58 +0000

    Add test_termios.c

---
+++ Linux/src/test/modules/test_termios.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <termios.h>
+#include <unistd.h>
+
+int test_termios(int argc, char* argv[])
+{
+    int c;
+    static struct termios oldt, newt;
+
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+    cfmakeraw(&newt);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+
+    while ((c = getchar()) != 27) {
+        putchar(c);
+    }
+
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+
+    return 0;
+}
--- Linux/src/test/test.c
+++ Linux/src/test/test.c
@@ -4,11 +4,11 @@
 #include "localize/localize.h"
 #include "test.h"
 
-extern int test_key_input(int, char*[]);
+extern int test_termios(int, char*[]);
 
 int test_module(int argc, char* argv[])
 {
     localize();
 
-    return test_key_input(argc, argv);
+    return test_termios(argc, argv);
 }
