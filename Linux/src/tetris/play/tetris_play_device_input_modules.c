be7c50c47e9441a5bcf790defac3d51876275518
keunbum <rkeunbum@gmail.com>
2023-04-26 00:53:00 +0900

    Fix bug - 키보드 장치 안닫히는 버그 해결.   기존 코드 그대로 복붙 하면서 생긴 인덱스 오류였음.

--- Linux/src/tetris/play/tetris_play_device_input_modules.c
+++ Linux/src/tetris/play/tetris_play_device_input_modules.c
@@ -1,14 +1,17 @@
+#include <assert.h>
+#include <stdbool.h>
+
 #include "tetris_play_device_input_modules.h"
 #include "tetris/tetris_play_manager.h"
 #include "tetris_play_device_input_module_keyboard.h"
 #include "tetris_play_device_input_module_controller.h"
 
-#define THREAD_MODULE_NUM (1)
+#define THREAD_MODULE_NUM (2)
 
 static void cleanup_device_input_modules(void* arg)
 {
     thread_module_t* const modules = (thread_module_t*)arg;
-    for (int i = 1; i < THREAD_MODULE_NUM; ++i) {
+    for (int i = 0; i < THREAD_MODULE_NUM; ++i) {
         int res = pthread_cancel(modules[i].thread_id);
         if (res != 0) {
             handle_error_en("pthread_cancel() error", res);
@@ -16,7 +19,7 @@ static void cleanup_device_input_modules(void* arg)
     }
 }
 
-static void callback_cleanup_device_input_modules_free(void* arg)
+static void callback_cleanup_device_input_modules(void* arg)
 {
     my_assert(arg != NULL);
 
@@ -33,14 +36,14 @@ void* mainfunc_device_input_modules(void* arg)
             .main_func = mainfunc_device_input_module_keyboard,
             .retval = NULL,
         },
-        // {
-        //     .is_detached = false,
-        //     .main_func = mainfunc_device_input_controller,
-        //     .retval = NULL,
-        // }
+        {
+            .is_detached = false,
+            .main_func = mainfunc_device_input_controller,
+            .retval = NULL,
+        }
     };
 
-    pthread_cleanup_push(callback_cleanup_device_input_modules_free, s_modules);
+    pthread_cleanup_push(callback_cleanup_device_input_modules, s_modules);
 
     for (int i = 0; i < THREAD_MODULE_NUM; ++i) {
         s_modules[i].main_func_arg = arg;
